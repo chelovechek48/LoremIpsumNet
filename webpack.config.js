dcd9719e931016a5b9cfb6ac5a414bea0952b648
Maksim Ivanyhin <chelovechek.maks@mail.ru>
2022-10-04 13:15:13 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+const CssPlugin = require('mini-css-extract-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const babelOptions = (preset) => {
+  const opts = {
+    presets: isProd ? ['@babel/preset-env'] : [],
+  };
+
+  if (preset) {
+    opts.presets.push(preset);
+  }
+
+  return opts;
+};
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    main: './js/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: isProd ? '[name].[contenthash].js' : '[name].js',
+    assetModuleFilename: isProd ? '[path][name].[contenthash][ext]' : '[path][name][ext]',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.js', '.sass', '.scss', '.css'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  optimization: {
+    chunkIds: 'named',
+  },
+  devServer: {
+    hot: false,
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: './index.html',
+      filename: isProd ? 'index.[contenthash].html' : 'index.html',
+    }),
+    new CssPlugin({
+      filename: isProd ? '[name].[contenthash].css' : '[name].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          CssPlugin.loader,
+          'css-loader',
+          'group-css-media-queries-loader',
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          CssPlugin.loader,
+          'css-loader',
+          'group-css-media-queries-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: 'asset',
+        // loader: 'url-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: babelOptions(),
+        },
+      },
+    ],
+  },
+};

86eb23eaad8d417954fc6c1792c0435735e8f527
Maksim Ivanyhin <chelovechek.maks@mail.ru>
2022-10-05 07:52:59 +0300

    added header block
    added JS functions: toggle/hide menu
    
    all added blocks:
    * button
    * container
    * hamburger
    * header
    * link
    * list
    * span

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,36 @@
 import '@/styles/style';
+
+const page = document.querySelector('.page');
+const logo = document.querySelector('.header__logo');
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.header__menu');
+
+const toggleMenu = () => {
+  page.classList.toggle('scroll-lock');
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('is-open');
+};
+const hideMenu = () => {
+  page.classList.remove('scroll-lock');
+  hamburger.classList.remove('active');
+  menu.classList.remove('is-open');
+};
+
+hamburger.addEventListener('click', () => {
+  toggleMenu();
+});
+
+window.addEventListener('scroll', () => {
+  hideMenu();
+});
+logo.addEventListener('click', () => {
+  hideMenu();
+});
+menu.addEventListener('click', () => {
+  hideMenu();
+});
+
+const resizeWindow = () => {
+  hideMenu();
+};
+window.addEventListener('resize', resizeWindow);
